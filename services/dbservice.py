f127cf2cd262d75e050cb3063f80246eaf434281
gmandell <guzvo92@gmail.com>
2021-01-22 22:21:52 -0600

    [v8/master] rutas configurado y constructortable

--- services/dbservice.py
+++ services/dbservice.py
@@ -126,19 +126,20 @@ class Maindb:
 
 dbg = Maindb()
 
-'''
-dbg.borrartabla('circuitos')
-dbg.creartabla('circuitos')
-dbg.insertnewdata('circuitos','c1','0','stringonG','stringoffG')
-dbg.insertnewdata('circuitos','c2','0','stringonG','stringoffG')
-dbg.insertnewdata('circuitos','c3','0','stringonG','stringoffG')
-dbg.insertnewdata('circuitos','c4','0','stringonG','stringoffG')
-dbg.insertnewdata('circuitos','c5','0','stringonG','stringoffG')
-dbg.insertnewdata('circuitos','c6','0','stringonG','stringoffG')
-dbg.insertnewdata('circuitos','c7','0','stringonG','stringoffG')
-dbg.insertnewdata('circuitos','c8','0','stringonG','stringoffG')
-dbg.insertnewdata('circuitos','c9','0','stringonG','stringoffG')
-dbg.insertnewdata('circuitos','c10','0','stringonG','stringoffG')
-'''
+from main import constructortable
+
+if constructortable == 1:
+    dbg.borrartabla('circuitos')
+    dbg.creartabla('circuitos')
+    dbg.insertnewdata('circuitos','c1','0','stringonG','stringoffG')
+    dbg.insertnewdata('circuitos','c2','0','stringonG','stringoffG')
+    dbg.insertnewdata('circuitos','c3','0','stringonG','stringoffG')
+    dbg.insertnewdata('circuitos','c4','0','stringonG','stringoffG')
+    dbg.insertnewdata('circuitos','c5','0','stringonG','stringoffG')
+    dbg.insertnewdata('circuitos','c6','0','stringonG','stringoffG')
+    dbg.insertnewdata('circuitos','c7','0','stringonG','stringoffG')
+    dbg.insertnewdata('circuitos','c8','0','stringonG','stringoffG')
+    dbg.insertnewdata('circuitos','c9','0','stringonG','stringoffG')
+    dbg.insertnewdata('circuitos','c10','0','stringonG','stringoffG')
 
 dbg.desco()
